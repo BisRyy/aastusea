9716fda1a2e9abc7885c82b0c9579c52ac3c747d
Bisrat Kebere <kiyyakebere@gmail.com>
2024-09-01 22:55:43 +0300

    feat: Blog with Contentlayer CMS

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+import { compareDesc, parseISO } from "date-fns";
+
+export const cx = (...classNames : any[]) => classNames.filter(Boolean).join(" ");
+
+export const sortBlogs = (blogs : any) => {
+  return blogs
+    .slice()
+    .sort((a : any, b : any) =>
+      compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt))
+    );
+};
