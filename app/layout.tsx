94a85e5bf099571796e329b862051bfde4956c09
Bisra Kebere <kiyyakebere@gmail.com>
2024-09-13 20:16:59 +0300

    feat: Add faviocons, analytics and speed insignts

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,13 @@ import {
 
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navbar } from "@/components/layout/navbar";
-
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -21,7 +22,8 @@ const fontSans = FontSans({
 
 export const metadata: Metadata = {
   title: "AASTU Software Engineers Association",
-  description: "A community of software engineers who are passionate about technology and innovation.",
+  description:
+    "A community of software engineers who are passionate about technology and innovation.",
 };
 
 export default function RootLayout({
@@ -46,15 +48,17 @@ export default function RootLayout({
           </SignedIn> */}
           <Toaster />
           <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {/* <Navbar /> */}
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* <Navbar /> */}
 
-          {children}
-        </ThemeProvider>
+            {children}
+          </ThemeProvider>
+          <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
