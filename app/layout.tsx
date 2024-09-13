1fbc770be4337665b13e6d23b30dc6ad075fc2bb
Bisra Kebere <kiyyakebere@gmail.com>
2024-09-13 21:36:40 +0300

    chore: SEO Optimizations

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 
-import type { Metadata } from "next";
+import type { Viewport, Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
@@ -15,15 +15,102 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navbar } from "@/components/layout/navbar";
+
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "#ffffff",
+};
+
 export const metadata: Metadata = {
   title: "AASTU Software Engineers Association",
   description:
     "A community of software engineers who are passionate about technology and innovation.",
+  metadataBase: new URL("https://aastu.software"),
+  openGraph: {
+    siteName: "AASTUSEA | AASTU Software Engineers Association",
+    type: "website",
+    locale: "en_US",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    "max-image-preview": "large",
+    "max-snippet": -1,
+    "max-video-preview": -1,
+    googleBot: "index, follow",
+  },
+  alternates: {
+    types: {
+      "application/rss+xml": "https://aastu.software/rss.xml",
+    },
+  },
+  applicationName: "AASTUSEA | AASTU Software Engineers Association",
+  appleWebApp: {
+    title: "AASTUSEA | AASTU Software Engineers Association",
+    statusBarStyle: "default",
+    capable: true,
+  },
+  verification: {
+    google: "YOUR_DATA",
+    yandex: ["YOUR_DATA"],
+    other: {
+      "msvalidate.01": ["YOUR_DATA"],
+      "facebook-domain-verification": ["YOUR_DATA"],
+    },
+  },
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        type: "image/x-icon",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    shortcut: [
+      {
+        url: "/favicon.ico",
+        type: "image/x-icon",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "57x57",
+        type: "image/png",
+      },
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "60x60",
+        type: "image/png",
+      },
+      // add apple-icon-72x72.png, apple-icon-76x76.png, apple-icon-114x114.png, apple-icon-120x120.png, apple-icon-144x144.png, apple-icon-152x152.png, apple-icon-180x180.png
+    ],
+  },
 };
 
 export default function RootLayout({
---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://aastu.software",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://aastu.software/learning",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://aastu.software/projects",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+  ];
+}
