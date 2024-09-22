cdcba72a16064f2ad68a6ed81eb693332b4eff39
Bisra Kebere <kiyyakebere@gmail.com>
2024-09-22 18:07:21 +0300

    chore: Add Partner Logos

--- components/layout/sections/sponsors.tsx
+++ components/layout/sections/sponsors.tsx
@@ -1,43 +1,73 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { Icon } from "@/components/ui/icon";
 import { Marquee } from "@devnomic/marquee";
 import "@devnomic/marquee/dist/index.css";
-import { icons } from "lucide-react";
-import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+
 interface sponsorsProps {
   icon: string;
   name: string;
+  id: number;
+  href: string;
+  image: string;
+  imageLight: string;
 }
 
 const sponsors: sponsorsProps[] = [
   {
+    id: 1,
+    href: "https://aastu.edu.et/",
+    icon: "Crown",
+    name: "AASTU",
+    image: "/logos/aastu.jpg",
+    imageLight: "/logos/aastu.jpg",
+  },
+  {
+    id: 2,
+    href: "https://microsoft.com?wt.mc_id=studentamb_410163",
     icon: "Crown",
     name: "Microsoft",
+    image: "/logos/mlsa.png",
+    imageLight: "/logos/mlsa.png",
   },
   {
+    id: 3,
+    href: "https://github.com/?wt.mc_id=studentamb_410163",
     icon: "Github",
     name: "Github",
+    image: "/logos/github.svg",
+    imageLight: "/logos/github-white.svg",
   },
   {
+    id: 4,
+    href: "https://ntn.so/bisratk",
     icon: "Ghost",
     name: "Notion",
+    image: "/logos/campus-leader-black.png",
+    imageLight: "/logos/campus-leader-white.png",
   },
   {
+    id: 5,
+    href: "https://efuyegela.com/",
     icon: "Puzzle",
     name: "Efuye Gela",
+    image: "/logos/efuye-gela.png",
+    imageLight: "/logos/efuye-gela.png",
   },
   {
+    id: 6,
+    href: "https://gdscaastu.club/",
     icon: "Squirrel",
     name: "GDSC AASTU",
+    image: "/logos/gdsc.png",
+    imageLight: "/logos/gdsc.png",
   },
 ];
 
 export const SponsorsSection = () => {
-  const { theme } = useTheme();
   return (
-    <section id="sponsors" className="container mx-auto p-12 ">
+    <section id="sponsors" className="container mx-auto p-12">
       <h1 className="text-xl md:text-3xl font-bold mb-8 text-center">
         Our Partners
       </h1>
@@ -49,18 +79,34 @@ export const SponsorsSection = () => {
           innerClassName="gap-[3rem]"
           pauseOnHover
         >
-          {sponsors.map(({ icon, name }) => (
+          {sponsors.map((brand) => (
             <div
-              key={name}
-              className="flex items-center text-xl md:text-2xl font-medium"
+              key={brand.id}
+              className="flex items-center justify-center w-[180px] h-[120px]"
             >
-              <Icon
-                name={icon as keyof typeof icons}
-                size={32}
-                color={theme === "dark" ? "white" : "black"}
-                 className="mr-2"
-              />
-              {name}
+              <Link
+                href={brand.href}
+                className="relative w-full h-full cursor-pointer"
+              >
+                <Image
+                  className="opacity-65 transition-all duration-300 hover:opacity-100 dark:hidden"
+                  src={brand.image}
+                  alt={brand.name}
+                  fill
+                  sizes="180px"
+                  style={{ objectFit: "contain" }}
+                  title={brand.name}
+                />
+                <Image
+                  className="hidden opacity-50 transition-all duration-300 hover:opacity-100 dark:block"
+                  src={brand.imageLight}
+                  alt={brand.name}
+                  fill
+                  sizes="180px"
+                  style={{ objectFit: "contain" }}
+                  title={brand.name}
+                />
+              </Link>
             </div>
           ))}
         </Marquee>
