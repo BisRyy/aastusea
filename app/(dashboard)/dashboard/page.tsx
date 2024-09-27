a192fc58491839569e0b41f7d416b95b4decd76b
Bisra Kebere <kiyyakebere@gmail.com>
2024-09-27 08:51:02 +0300

    feat: Add Linkedin sharing

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -5,20 +5,26 @@ import { currentUser } from "@clerk/nextjs/server";
 import { CourseList } from "@/components/sections/learning/CourseList";
 import { AllCourses } from "@/data/courses";
 import CourseTable from "../../../components/sections/dashboard/course-table";
-import { LoaderIcon } from "lucide-react";
+import { LoaderIcon, ShareIcon } from "lucide-react";
 import Greeting from "../../../components/sections/dashboard/greeting";
 import { Button } from "@/components/ui/button";
-import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
-import { Tooltip, TooltipProvider, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip";
+import { QuestionMarkCircledIcon, Share1Icon } from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
 
 export default async function Page() {
+  const linkedinShareUrl = process.env.NEXT_PUBLIC_LINKEDIN_SHARE_URL;
   return (
     <div className="flex h-full gap-2 m-4">
       <Redirect />
       <div className="flex-grow">
-        <div className="flex items-start justify-between mx-4 my-4">
+        <div className="flex items-start justify-between m-4">
           <Greeting />
-          <div className="flex items-center gap-2">
+          <div className="flex items-end justify-end gap-2">
             <TooltipProvider>
               <Tooltip>
                 <TooltipTrigger asChild>
@@ -26,9 +32,21 @@ export default async function Page() {
                     <QuestionMarkCircledIcon className="w-4 h-4" />
                   </Button>
                 </TooltipTrigger>
-                <TooltipContent className="text-xs">This website is in active development</TooltipContent>
+                <TooltipContent className="text-xs">
+                  This website is in active development
+                </TooltipContent>
               </Tooltip>
             </TooltipProvider>
+            <Button className="rounded-full flex gap-2" asChild>
+              <a
+                href={linkedinShareUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="hidden md:block">Spread the word</span>
+                <Share1Icon className="w-4 h-4" />
+              </a>
+            </Button>
           </div>
         </div>
         <CourseTable courses={AllCourses} />
